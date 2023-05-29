f9b4ab015bace8da9e2e69996de6f2f4102a3dd9
Max <maksonster99@gmail.com>
2023-05-29 17:21:09 +0600

    change gendiff.js

--- gendiff.js
+++ gendiff.js
@@ -1,11 +1,15 @@
+#!/usr/bin/env node
 import { program } from 'commander';
 
 program
 .name('gendiff')
-//  version
+.version('0.0.2')
 .description('Compares two configuration files and shows a difference.')
-.option('-V, --version', 'output the version number')
-.option('-h, --help', 'output usage information')
+.arguments('<filepath1> <filepath2>', 'filepathes to configs')
+//  .option('-V, --version', 'output the version number')
+//  .option('-h, --help', 'output usage information')
+.option('-f, --format <type>', 'output format')
+//  .argument('<filepath2>', 'filepath to the second config')
 .parse(process.argv);
 
 console.log(program.helpInformation());
