dfe4ce865914c614043f757492c3d52c6579ecc8
Max <maksonster99@gmail.com>
2023-06-06 13:17:41 +0600

    tried to clean CC

--- __tests__/genDiff-func.test.js
+++ __tests__/genDiff-func.test.js
@@ -1,5 +1,7 @@
 import genDiff from '../genDiff-func.js';
 
+const commonObj = { a: 1, b: 2 };
+
 describe('genDiff', () => { // название теста
   it('возвращает пустую строку, когда оба объекта пустые', () => { // название проверки
     const obj1 = {};
@@ -12,7 +14,7 @@ describe('genDiff', () => { // название теста
   });
 
   it('возвращает строку с разными и с одинаковыми значениями (2 шт.)', () => {
-    const obj1 = { a: 1, b: 2 };
+    const obj1 = commonObj;
     const obj2 = { a: 3, b: 2 };
     const keys = ['a', 'b'];
 
@@ -23,7 +25,7 @@ describe('genDiff', () => { // название теста
   });
 
   it('возвращает строку с разными и с одинаковыми значениями (3 шт.)', () => {
-    const obj1 = { a: 1, b: 2 };
+    const obj1 = commonObj;
     const obj2 = { b: 2, c: 3 };
     const keys = ['a', 'b', 'c'];
 
@@ -34,7 +36,7 @@ describe('genDiff', () => { // название теста
   });
 
   it('возвращает строку со всеми отличительными значениями, когда все значения разные', () => {
-    const obj1 = { a: 1, b: 2 };
+    const obj1 = commonObj;
     const obj2 = { b: 3, c: 4 };
     const keys = ['a', 'b', 'c'];
 
@@ -45,8 +47,8 @@ describe('genDiff', () => { // название теста
   });
 
   it('возвращает все строки, когда все файлы одинаковые', () => {
-    const obj1 = { a: 1, b: 2 };
-    const obj2 = { a: 1, b: 2 };
+    const obj1 = commonObj;
+    const obj2 = commonObj;
     const keys = ['a', 'b'];
 
     const result = genDiff(obj1, obj2, keys);
