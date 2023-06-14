d7a3b00dc286707e632124edea36f9eaffcdffce
Max <maksonster99@gmail.com>
2023-06-14 13:20:33 +0600

    i have done stylish. now it works with any objects!

--- bin/stylish.js
+++ bin/stylish.js
@@ -1,40 +1,42 @@
 import _ from 'lodash';
 
-const indent = 4;
+const startIndent = 4;
 const replacer = ' ';
+const genIndent = (level, isOffset = false) => {
+  const offset = (isOffset) ? 2 : 0;
+  return replacer.repeat(startIndent * level - offset);
+};
 
-const stringify = (value, level) => {
-  const newIndent = replacer.repeat(indent * level);
-
+const stringify = (value, level = 1) => {
   if (!_.isObject(value)) {
     return String(value);
   }
   const lines = Object.entries(value)
-    .map(([key, value]) => `${newIndent}${key}: ${stringify(value, level + 1)}\n`)
+    .map(([key, value]) => `${genIndent(level)}${key}: ${stringify(value, level + 1)}`)
     .join('\n');
-    return `{\n${lines}${newIndent}\n}`;
+  return `{\n${lines}\n${genIndent(level - 1)}}`;
 };
 
-const iter = (data) => {
-    return data.map (obj => {
-      const { key, value, status } = obj;
+const iter = (data, level = 1) => {
+  return data.map(obj => {
+    const { key, value, status } = obj;
 
-      if (status === 'deleted') {
-        return `- ${key}: ${stringify(value, 0)}\n`;
-      }
-      if (status === 'added') {
-        return `+ ${key}: ${stringify(value, 0)}\n`;
-      }
-      if (status === 'unchanged') {
-        return `  ${key}: ${stringify(value, 0)}\n`;
-      }
-      if (status === 'changed') {
-        return `- ${key}: ${stringify(value[0], 0)}\n+ ${key}: ${stringify(value[1], 0)}\n`;
-      }
-      if (status === 'nested') {
-        return `${key}: ${iter(value)}\n`;
-      }
-    }).join('');
-  };
+    if (status === 'deleted') {
+      return `${genIndent(level, true)}- ${key}: ${stringify(value, level + 1)}`;
+    }
+    if (status === 'added') {
+      return `${genIndent(level, true)}+ ${key}: ${stringify(value, level + 1)}`;
+    }
+    if (status === 'unchanged') {
+      return `${genIndent(level, true)}  ${key}: ${stringify(value, level + 1)}`;
+    }
+    if (status === 'changed') {
+      return `${genIndent(level, true)}- ${key}: ${stringify(value[0], level + 1)}\n${genIndent(level, true)}+ ${key}: ${stringify(value[1], level + 1)}`;
+    }
+    if (status === 'nested') {
+      return `${genIndent(level)}${key}: {\n${iter(value, level + 1)}\n${genIndent(level)}}`;
+    }
+  }).join('\n');
+};
 
-  export default iter;
+export default (data) => `{\n${iter(data)}\n}`;
