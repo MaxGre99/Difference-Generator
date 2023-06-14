3f854c0c82a1a22f4f72ff61be748cfcc6653dd1
Max <maksonster99@gmail.com>
2023-06-14 15:42:04 +0600

    more stylish.js refactoring

--- bin/stylish.js
+++ bin/stylish.js
@@ -11,34 +11,34 @@ const stringify = (value, level = 1) => {
   if (!_.isObject(value)) {
     return String(value);
   }
+
   const lines = Object.entries(value)
     .map(([key, value]) => `${genIndent(level)}${key}: ${stringify(value, level + 1)}`)
     .join('\n');
+
   return `{\n${lines}\n${genIndent(level - 1)}}`;
 };
 
 const iter = (data, level = 1) => {
-  return data.map(obj => {
+  const result = [];
+  for (const obj of data) {
     const { key, value, status } = obj;
     const standartIndent = genIndent(level, true);
     const stringValue = stringify(value, level + 1);
 
     if (status === 'deleted') {
-      return `${standartIndent}- ${key}: ${stringValue}`;
+      result.push(`${standartIndent}- ${key}: ${stringValue}`);
+    } else if (status === 'added') {
+      result.push(`${standartIndent}+ ${key}: ${stringValue}`);
+    } else if (status === 'unchanged') {
+      result.push(`${standartIndent}  ${key}: ${stringValue}`);
+    } else if (status === 'changed') {
+      result.push(`${standartIndent}- ${key}: ${stringify(value[0], level + 1)}\n${standartIndent}+ ${key}: ${stringify(value[1], level + 1)}`);
+    } else if (status === 'nested') {
+      result.push(`${genIndent(level)}${key}: {\n${iter(value, level + 1)}\n${genIndent(level)}}`);
     }
-    if (status === 'added') {
-      return `${standartIndent}+ ${key}: ${stringValue}`;
-    }
-    if (status === 'unchanged') {
-      return `${standartIndent}  ${key}: ${stringValue}`;
-    }
-    if (status === 'changed') {
-      return `${standartIndent}- ${key}: ${stringify(value[0], level + 1)}\n${standartIndent}+ ${key}: ${stringify(value[1], level + 1)}`;
-    }
-    if (status === 'nested') {
-      return `${genIndent(level)}${key}: {\n${iter(value, level + 1)}\n${genIndent(level)}}`;
-    }
-  }).join('\n');
+  }
+  return result.join('\n');
 };
 
 export default (data) => `{\n${iter(data)}\n}`;
