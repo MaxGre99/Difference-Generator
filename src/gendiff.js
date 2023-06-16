0f82af4fd7ae427cd09b8b46a4b8c24e3adf7445
Max <maksonster99@gmail.com>
2023-06-16 16:52:46 +0600

    check #3

--- src/gendiff.js
+++ src/gendiff.js
@@ -5,15 +5,15 @@ import genStatus from './genStatus.js';
 import parsers from './parsers.js';
 import formatter from './formatters/index.js';
 
-export default (filepath1, filepath2, formatName) => {
+export default (filepath1, filepath2, formatName = 'stylish') => {
   const resolvedPath1 = path.resolve(process.cwd(), filepath1);
   const resolvedPath2 = path.resolve(process.cwd(), filepath2);
 
   const file1 = fs.readFileSync(resolvedPath1, 'utf-8'); // создаём строки с наполнением из файла
   const file2 = fs.readFileSync(resolvedPath2, 'utf-8');
 
-  const fileExtension1 = path.extname(resolvedPath1);
-  const fileExtension2 = path.extname(resolvedPath2);
+  const fileExtension1 = path.extname(resolvedPath1).slice(1);
+  const fileExtension2 = path.extname(resolvedPath2).slice(1);
 
   const obj1 = parsers(fileExtension1, file1);
   const obj2 = parsers(fileExtension2, file2);
--- src/parsers.js
+++ src/parsers.js
@@ -1,10 +1,10 @@
 import yaml from 'js-yaml';
 
 export default (fileExtension, file) => {
-  if (fileExtension === '.json') {
+  if (fileExtension === 'json') {
   const obj = JSON.parse(file); // превращаем строки в объекты (ключ: значение)
   return obj;
-  } else if (fileExtension === '.yaml' || fileExtension === '.yml') {
+  } else if (fileExtension === 'yaml' || fileExtension === 'yml') {
     const obj = yaml.load(file);
     return obj;
   } else {
